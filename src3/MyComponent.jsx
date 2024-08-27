f1153a185e6c4d69d2ae828435e951870a0bffa1
Farid Shahidi <shahidi@innoscripta.com>
2024-08-27 11:41:20 +0200

    implement

---
+++ src3/MyComponent.jsx
@@ -0,0 +1,8 @@
+export const MyComponent = (props) => {
+  const { label } = props;
+  return (
+    <div>
+      <button>xxxx {label}</button>
+    </div>
+  );
+};
---
+++ src3/index.js
@@ -0,0 +1,7 @@
+import { MyComponent } from "./MyComponent";
+
+const sayHello = (x) => {
+  alert(1 + x);
+};
+
+export { MyComponent, sayHello };
